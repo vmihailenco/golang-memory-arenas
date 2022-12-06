3e88fc3cd3054e214fd401f8d434ee04fd476d2b
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-12-06 17:00:27 +0200

    Add memory arenas

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
-// This is unmodified Go #2 binary tree (current fastest Go program) from Benchmarks Game:
+// This is a MODIFIED Go #2 binary tree from the Benchmarks Game:
 //   https://benchmarksgame-team.pages.debian.net/benchmarksgame/program/binarytrees-go-2.html
 //
-//
-// The only change is to default to a tree depth if 21 if not specified on command line.
+// Modifications include:
+//  * adding arenas support
+//  * -minalloc flag controls how frequently each worker goroutine calls Free
+//  * -single flag creates 1 tree in 1 goroutine
+//  * -cpuprofile and -memprofile flags for pprof
+//  * default to binary tree depth of 21 if not specified via command line
+//  * slightly modified output
 //
 // License is 3-Clause BSD:
 //   https://benchmarksgame-team.pages.debian.net/benchmarksgame/license.html
 //
+// Leaving the following comments as-is from the original Go #2 program:
 // -------------------------------------------------------
 //
 // The Computer Language Benchmarks Game
@@ -49,11 +55,25 @@
 package main
 
 import (
+	"arena"
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
 	"strconv"
 	"sync"
+)
+
+// minalloc flag controls how frequently each worker goroutine calls Free
+var minAllocMB = flag.Float64("minalloc", 1, "upon completing a tree, a worker goroutine "+
+	"reuses its arena unless the arena has completed more than minalloc `MB` of allocations")
+var single = flag.Bool("single", false, "allocate one tree in a single goroutine")
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 )
 
 type Tree struct {
@@ -71,16 +91,33 @@ func (t *Tree) Count() int {
 }
 
 // Create a complete binary tree of `depth` and return it as a pointer.
-func NewTree(depth int) *Tree {
+func NewTree(depth int, a *arena.Arena) *Tree {
+	// thepudds: alloc via an arena if we have one.
 	if depth > 0 {
-		return &Tree{Left: NewTree(depth - 1), Right: NewTree(depth - 1)}
+		// thepudds: note that for this particular benchmark, it is faster to create the
+		// left and right sub-trees before allocating our own tree node.
+		// Otherwise, we could eliminate a couple of lines here.
+		left := NewTree(depth-1, a)
+		right := NewTree(depth-1, a)
+		treePtr := allocTreeNode(a)
+		treePtr.Left = left
+		treePtr.Right = right
+		return treePtr
+	} else {
+		return allocTreeNode(a)
+	}
+}
+
+// Allocate an empty tree node, using an arena if provided.
+func allocTreeNode(a *arena.Arena) *Tree {
+	if a != nil {
+		return arena.New[Tree](a)
 	} else {
 		return &Tree{}
 	}
 }
 
 func Run(maxDepth int) {
-
 	var wg sync.WaitGroup
 
 	// Set minDepth to 4 and maxDepth to the maximum of maxDepth and minDepth +2.
@@ -98,21 +135,39 @@ func Run(maxDepth int) {
 	// first position of the outputBuffer with its statistics.
 	wg.Add(1)
 	go func() {
-		tree := NewTree(maxDepth + 1)
-		msg := fmt.Sprintf("stretch tree of depth %d\t check: %d",
+		// thepudds: create a single arena for this single (usually large) tree,
+		// freeing it when we are done with this tree.
+		stretchArena := arena.NewArena()
+		defer stretchArena.Free()
+
+		tree := NewTree(maxDepth+1, stretchArena)
+		nodes := tree.Count()
+		msg := fmt.Sprintf("   stretch tree of depth %-8d arenas: %-6d nodes: %-10d MB: %0.1f",
 			maxDepth+1,
-			tree.Count())
+			1,
+			nodes,
+			float64(nodes*16)/(1<<20))
 
 		outBuff[0] = msg
 		wg.Done()
 	}()
+	if *single {
+		// thepudds: only do a single tree (with only one goroutine)
+		wg.Wait()
+		return
+	}
 
 	// Create a long-lived binary tree of depth maxDepth. Its statistics will be
 	// handled later.
 	var longLivedTree *Tree
 	wg.Add(1)
+	// thepudds: also create a long-lived arena for this long-lived tree,
+	// freeing it when we are done with this function.
+	longLivedArena := arena.NewArena()
+	defer longLivedArena.Free()
+
 	go func() {
-		longLivedTree = NewTree(maxDepth)
+		longLivedTree = NewTree(maxDepth, longLivedArena)
 		wg.Done()
 	}()
 
@@ -124,19 +179,38 @@ func Run(maxDepth int) {
 
 		wg.Add(1)
 		go func(depth, iterations, index int) {
-			acc := 0
+			// Create a binary tree of depth and accumulate total counter with its
+			// node count.
+
+			// thepudds: Also create an arena for the binary tree allocations for this goroutine.
+			// We reuse each arena until it has allocated more than minAllocMB.
+			treeArena := arena.NewArena()
+			arenaCount := 1
+			allocated := 0
+
+			nodes := 0
 			for i := 0; i < iterations; i++ {
-				// Create a binary tree of depth and accumulate total counter with its
-				// node count.
-				a := NewTree(depth)
-				acc += a.Count()
+				if allocated > int(*minAllocMB*(1<<20)) {
+					treeArena.Free()
+					treeArena = arena.NewArena()
+					arenaCount++
+					allocated = 0
+				}
+				tree := NewTree(depth, treeArena)
+				newNodes := tree.Count()
+				nodes += newNodes
+				allocated += newNodes * 16
 			}
-			msg := fmt.Sprintf("%d\t trees of depth %d\t check: %d",
+
+			msg := fmt.Sprintf(" %8d trees of depth %-8d arenas: %-6d nodes: %-10d MB: %0.1f",
 				iterations,
 				depth,
-				acc)
-
+				arenaCount,
+				nodes,
+				float64(nodes*16)/(1<<20))
 			outBuff[index] = msg
+
+			treeArena.Free()
 			wg.Done()
 		}(depth, iterations, outCurr)
 	}
@@ -145,9 +219,12 @@ func Run(maxDepth int) {
 
 	// Compute the checksum of the long-lived binary tree that we created
 	// earlier and store its statistics.
-	msg := fmt.Sprintf("long lived tree of depth %d\t check: %d",
+	nodes := longLivedTree.Count()
+	msg := fmt.Sprintf("long lived tree of depth %-8d arenas: %-6d nodes: %-10d MB: %0.1f",
 		maxDepth,
-		longLivedTree.Count())
+		1,
+		nodes,
+		float64(nodes*16)/(1<<20))
 	outBuff[outSize-1] = msg
 
 	// Print the statistics for all of the various tree depths.
@@ -158,6 +235,32 @@ func Run(maxDepth int) {
 
 func main() {
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	if *memprofile != "" {
+		defer func() {
+			f, err := os.Create(*memprofile)
+			if err != nil {
+				log.Fatal("could not create memory profile: ", err)
+			}
+			defer f.Close()
+			runtime.GC() // get up-to-date statistics
+			if err := pprof.WriteHeapProfile(f); err != nil {
+				log.Fatal("could not write memory profile: ", err)
+			}
+		}()
+	}
 
 	n := 21
 	if flag.NArg() > 0 {
